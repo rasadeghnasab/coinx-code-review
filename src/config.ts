42da0b83a2cabdd79d0aff243d2687bbd949f296
Ramin <rasadeghnasab@gmail.com>
2022-02-01 03:49:37 +0330

    - Initial commit

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const checkEnv = (envVar: string, defaultValue?: string) => {
+  if (!process.env[envVar]) {
+    if (defaultValue) {
+      return defaultValue;
+    }
+    throw new Error(`Please define the Enviroment variable"${envVar}"`);
+  } else {
+    return process.env[envVar] as string;
+  }
+};
+
+export const PORT: number = parseInt(checkEnv("PORT", '3000'), 10);
+export const DBURL: string = checkEnv("DBURL", "dburl");
+export const CORS_ORIGINS = [`http://localhost:${PORT}`];
